dd8aa98d8c017b5b6cd83aeb70c408fa3407250e
Sabrina Paschini <sabrina.paschini@hotmail.com>
2024-06-26 15:54:03 -0300

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 
 
+
 export class AppComponent {
   title = 'mytodolist';
 
-  
+   list:string[] = ['angular1'];
 
-  let list:string[] = ['angular'];
 }
